04b39432be7547199b21a879de5bba7cdf77aeed
Nahuel Gallegos <nahuel.galle93@gmail.com>
2021-03-01 18:49:37 -0300

    Ejercicio Universidad

---
+++ practicaUniversidad.js
@@ -0,0 +1,30 @@
+function probarEjercicio()
+{
+	/*2. Se desconoce la cantidad de alumnos que se ingresaran.
+3. Se deberán validar los casos resaltados en negrita.
+4. El programa deberá informar a través del documento html:
+A. Cantidad total de alumnos de cada carrera.
+B. Cantidad total de mujeres que cursan la carrera de programación
+C. Cantidad de alumnos no binarios.
+D. Promedio de notas de los alumnos finalizantes.
+E. Nombre, sexo y edad del alumno mas viejo en la carrera de psicología.
+F. Nombre, nota y estado de la carrera del mejor alumno no binario que estudia psicología.)
+G. ¿Cuál es la carrera que tiene más alumnos?
+*/
+	var nombre;
+	var carrera;
+	var estadoCarrera;
+	var sexo;
+	var edad;
+	var nota;
+
+	var CantidadTotal;
+	var cantidadMujeresProgramacion;
+	var cantidadNoBinarios;
+	var promedioFinalizantes;
+	
+	nombre = prompt ("Nombre:");
+	carrera = prompt ("Carrera:");
+	estadoCarrera = prompt("Estado carrera? Ingrese C para en curso, A para abandonado, o F para finalizado")
+}
+		
